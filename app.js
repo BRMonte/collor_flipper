8eea66aa30571fad8eeea29620cc40fb62f94044
Bruno Montebello <brunormontebello@hotmail.com>
2021-01-18 21:36:00 +0000

    Create randomNumber function

--- app.js
+++ app.js
@@ -5,7 +5,13 @@ const color = document.querySelector('.color');
 
 btn.addEventListener('click', function () {
   //get random number between 0 and 3 -> array colors
-    const randomNumber = 2;
+    const randomNumber = getRandomNumber();
+    console.log(randomNumber);
+
     document.body.style.backgroundColor = colors[randomNumber];
     color.textContent = colors[randomNumber];
 });
+
+function getRandomNumber() {
+  return Math.floor(Math.random() * colors.length); //forma de fazer c q a geracao de random numbers fique limitada ao numero de elementos do array + arrendondada
+}
